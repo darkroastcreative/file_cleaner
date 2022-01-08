012ff7fffe4fc4366c180f8ea9378d483ededa94
David J. Fink <david@darkroastcreative.co>
2022-01-07 17:47:58 -0700

    Updated Logic to Ensure All Files Deleted

--- bin/file_cleaner.dart
+++ bin/file_cleaner.dart
@@ -33,10 +33,17 @@ void main(List<String> arguments) async {
           .then((list) => paths = list);
 
       // Sort List of paths alphabetically.
-      paths.sort((a, b) => a.path.compareTo(b.path));
+      paths.sort((a, b) => a.path.length.compareTo(b.path.length));
 
       print(
-          'Paths list built. Proceeding to spawn threads to initiate delete operation.');
+          'Paths list built with ${paths.length} items. Proceeding to spawn threads to initiate delete operation.');
+
+      List<Isolate> threads = [];
+      List<int> terminatedThreads = [];
+      ReceivePort receivePort = ReceivePort();
+      receivePort.listen((message) {
+        terminatedThreads.add(message);
+      });
 
       if (paths.length >= numThreads) {
         int pathsPerThread = (paths.length / numThreads).ceil();
@@ -47,8 +54,10 @@ void main(List<String> arguments) async {
           print(
               'Spawning thread to delete ${paths.sublist(startIndex, endIndex).length} files.');
 
-          Isolate.spawn(
-              file_cleaner.deleteFiles, [paths.sublist(startIndex, endIndex)]);
+          Isolate.spawn(file_cleaner.deleteFiles, [
+            paths.sublist(startIndex, endIndex)
+          ]).then((isolate) =>
+              file_cleaner.prepareIsolate(isolate, threads, receivePort));
 
           startIndex = endIndex;
           endIndex = startIndex + pathsPerThread;
@@ -58,21 +67,34 @@ void main(List<String> arguments) async {
           }
         }
       } else {
-        Isolate.spawn(file_cleaner.deleteFiles, [paths]);
+        print('Spawning thread to delete ${paths.length} files.');
+        Isolate.spawn(file_cleaner.deleteFiles, [paths]).then((isolate) =>
+            file_cleaner.prepareIsolate(isolate, threads, receivePort));
       }
 
-      // print('Files in targeted path have been deleted.');
-      // print('Proceeding to delete target path.');
+      while (threads.isNotEmpty) {
+        threads.forEach((thread) {
+          if (terminatedThreads.contains(thread.hashCode)) {
+            threads.remove(thread);
+          }
+        });
+      }
 
-      // // Delete target directory.
+      print('Files in targeted path have been deleted.');
+      print('Proceeding to delete target path.');
+
+      // Delete target directory.
       // Directory(passedArgs['path']).deleteSync(recursive: true);
 
-      // print('Targeted path deleted. Delete operation complete.');
+      print('Targeted path deleted. Delete operation complete. ✅');
     } catch (exception) {
       print(exception);
+      exit(1);
     }
   } else {
     print(
         'ERROR: No folder path provided. Please provide a valid folder path with the "-p" argument when running.');
   }
+
+  exit(0);
 }
