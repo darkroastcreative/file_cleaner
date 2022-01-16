772ff2677af79ddc9649c0e9d92c87267e89bb1f
David J. Fink <david@darkroastcreative.co>
2022-01-16 16:59:53 -0700

    Updated Core Logic to More Reliably Delete Files, Improve Delete Performance

--- lib/file_cleaner.dart
+++ lib/file_cleaner.dart
@@ -1,25 +1,11 @@
 import 'dart:io';
 
-import 'dart:isolate';
-
-bool deleteFile(FileSystemEntity path) {
-  if (path.existsSync()) {
-    path.deleteSync(recursive: true);
-
-    return !path.existsSync();
-  }
-
-  return true;
+void deleteFile(FileSystemEntity path) {
+  path.exists().then((exists) => {
+        if (exists) {path.delete()}
+      });
 }
 
 void deleteFiles(List message) {
   message[0].forEach((path) => deleteFile(path));
-}
-
-void prepareIsolate(
-    Isolate thread, List<Isolate> threads, ReceivePort receivePort) {
-  Capability pauseCapability = thread.pause();
-  thread.addOnExitListener(receivePort.sendPort, response: thread.hashCode);
-  threads.add(thread);
-  thread.resume(pauseCapability);
 }
